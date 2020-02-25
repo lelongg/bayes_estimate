170e4e25c904c7296daa83da257b596054e1862e
Michael Stevens <mail@michael-stevens.de>
2020-02-25 08:31:09 +0100

    Use generic Storage

--- src/mine/matrix.rs
+++ src/mine/matrix.rs
@@ -1,72 +1,11 @@
 use nalgebra as na;
 
-use na::{RealField, Dim, DefaultAllocator, allocator::Allocator, Matrix, Vector, Scalar, MatrixN, MatrixMN};
-use na::base::storage::{Storage, StorageMut};
-use nalgebra::{SquareMatrix, U1};
-use nalgebra::base::constraint::{ShapeConstraint, DimEq, SameNumberOfRows};
+use na::{RealField, Dim, DefaultAllocator, allocator::Allocator, Matrix, Vector, Scalar, MatrixN, MatrixMN, SquareMatrix};
+use na::storage::{Storage, StorageMut};
+use nalgebra::constraint::{ShapeConstraint, DimEq};
 
 
 /// Computes the quadratic form `self = alpha * lhs * mid * lhs.transpose() + beta * self`.
-///
-/// This uses the provided workspace `work` to avoid allocations for intermediate results.
-///
-/// # Examples:
-///
-/// ```
-/// # #[macro_use] extern crate approx;
-/// # use nalgebra::{DMatrix, DVector};
-/// // Note that all those would also work with statically-sized matrices.
-/// // We use DMatrix/DVector since that's the only case where pre-allocating the
-/// // workspace is actually useful (assuming the same workspace is re-used for
-/// // several computations) because it avoids repeated dynamic allocations.
-/// let mut mat = DMatrix::identity(2, 2);
-/// let lhs = DMatrix::from_row_slice(2, 3, &[1.0, 2.0, 3.0,
-///                                           4.0, 5.0, 6.0]);
-/// let mid = DMatrix::from_row_slice(3, 3, &[0.1, 0.2, 0.3,
-///                                           0.5, 0.6, 0.7,
-///                                           0.9, 1.0, 1.1]);
-/// // The random shows that values on the workspace do not
-/// // matter as they will be overwritten.
-/// let mut workspace = DVector::new_random(2);
-/// let expected = &lhs * &mid * lhs.transpose() * 10.0 + &mat * 5.0;
-///
-/// mat.quadform_tr_with_workspace(&mut workspace, 10.0, &lhs, &mid, 5.0);
-/// assert_relative_eq!(mat, expected);
-pub fn quadform_tr_with_workspace<N: RealField, D1: Dim, S: StorageMut<N, D1, D1>, D2, S2, R3, C3, S3, D4, S4>(
-    mat: &mut SquareMatrix<N, D1, S>,
-    work: &mut Vector<N, D2, S2>,
-    alpha: N,
-    lhs: &Matrix<N, R3, C3, S3>,
-    mid: &Vector<N, D4, S4>,
-    beta: N,
-) where
-    D2: Dim,
-    R3: Dim,
-    C3: Dim,
-    D4: Dim,
-    S2: StorageMut<N, D2>,
-    S3: Storage<N, R3, C3>,
-    S4: Storage<N, D4>,
-    ShapeConstraint: DimEq<D1, D2> + DimEq<D1, R3> + DimEq<D2, R3> + DimEq<C3, D4>
-         + SameNumberOfRows<D2,R3>
-{
-    work.copy_from::<R3, U1, _>(&lhs.column(0));
-    *work *= mid[0];
-    mat.ger(alpha, work, &lhs.column(0), beta);
-
-    for j in 1..mid.nrows() {
-        work.copy_from(&lhs.column(j));
-        *work *= mid[j];
-        mat.ger(alpha, work, &lhs.column(j), N::one());
-    }
-}
-
-
-/// Computes the quadratic form `self = alpha * lhs * mid * lhs.transpose() + beta * self`.
-///
-/// This allocates a workspace vector of dimension D1 for intermediate results.
-/// If `D1` is a type-level integer, then the allocation is performed on the stack.
-/// Use `.quadform_tr_with_workspace(...)` instead to avoid allocations.
 ///
 /// # Examples:
 ///
@@ -81,40 +20,54 @@ pub fn quadform_tr_with_workspace<N: RealField, D1: Dim, S: StorageMut<N, D1, D1
 ///
 /// quadform_tr(&mat, 10.0, &lhs, &mid, 5.0);
 /// assert_relative_eq!(mat, expected);
-pub fn quadform_tr<N, D1: Dim, S: StorageMut<N, D1, D1>, R3, C3, S3, D4, S4>(
+pub fn quadform_tr<N: RealField, D1, S, R3, C3, S3, D4, S4>(
     mat: &mut SquareMatrix<N, D1, S>,
     alpha: N,
     lhs: &Matrix<N, R3, C3, S3>,
     mid: &Vector<N, D4, S4>,
     beta: N,
 ) where
-    N: RealField,
+    D1: Dim,
+    S: StorageMut<N, D1, D1>,
     R3: Dim,
     C3: Dim,
     D4: Dim,
     S3: Storage<N, R3, C3>,
     S4: Storage<N, D4>,
-    ShapeConstraint: DimEq<D1, D1> + DimEq<D1, R3> + DimEq<C3, D4>,
-    DefaultAllocator: Allocator<N, R3>
+    ShapeConstraint: DimEq<D1, R3> + DimEq<C3, D4>
 {
-    let mut work = unsafe { Vector::new_uninitialized_generic(lhs.data.shape().0, U1) };
-    quadform_tr_with_workspace(mat, &mut work, alpha, lhs, mid, beta)
+    mat.ger(alpha * mid[0], &lhs.column(0), &lhs.column(0), beta);
+
+    for j in 1..mid.nrows() {
+        mat.ger(alpha * mid[j], &lhs.column(j), &lhs.column(j), N::one());
+    }
 }
 
 
-pub fn prod_spd<N: RealField, R: Dim, C: Dim>(x: &MatrixMN<N, R, C>, s: &MatrixN<N, C>) -> MatrixN<N, R>
+pub fn prod_spd<N: RealField, R3, C3, D4, S3, S4>(lhs: &Matrix<N, R3, C3, S3>, mid: &SquareMatrix<N, D4, S4>) -> MatrixN<N, R3>
     where
-        DefaultAllocator: Allocator<N, R, C> + Allocator<N, C, R> + Allocator<N, R, R> + Allocator<N, C, C>
+        R3: Dim,
+        C3: Dim,
+        S3: Storage<N, R3, C3>,
+        D4: Dim,
+        S4: Storage<N, D4, D4>,
+        DefaultAllocator: Allocator<N, R3, D4> + Allocator<N, C3, R3> + Allocator<N, R3, R3>,
+        ShapeConstraint: DimEq<C3, D4> + DimEq<D4, C3>
 {
-    x * s * x.transpose()
+    lhs * mid * lhs.transpose()
 }
 
-pub fn prod_spdt<N: RealField, R: Dim, C: Dim>(x: &MatrixMN<N, R, C>, s: &MatrixN<N, R>) -> MatrixN<N, C>
+pub fn prod_spdt<N: RealField, R3, C3, D4, S3, S4>(lhs: &Matrix<N, R3, C3, S3>, mid: &SquareMatrix<N, D4, S4>) -> MatrixN<N, C3>
     where
-        DefaultAllocator:
-        Allocator<N, R, C> + Allocator<N, C, R> + Allocator<N, R, R> + Allocator<N, C, C>
+        R3: Dim,
+        C3: Dim,
+        S3: Storage<N, R3, C3>,
+        D4: Dim,
+        S4: Storage<N, D4, D4>,
+        DefaultAllocator: Allocator<N, D4, C3> + Allocator<N, C3, D4> + Allocator<N, C3, C3>,
+        ShapeConstraint: DimEq<D4, R3>
 {
-    (s * x).transpose() * x
+    (mid * lhs).transpose() * lhs
 }
 
 pub fn as_zeros<N: RealField, R: Dim, C: Dim>(shape: (R, C)) -> MatrixMN<N, R, C>
